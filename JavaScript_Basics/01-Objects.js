b8ed2b4fedaafc631f2c2bcdf3ec597802e4c5ee
me_dheeraj <9442273+Dheerajmadhukar@users.noreply.github.com>
2021-09-11 15:50:15 +0530

    Create 01-Objects.js

---
+++ JavaScript_Basics/01-Objects.js
@@ -0,0 +1,30 @@
+var object = {
+    "name": "Dheeraj Madhukar",
+    "twitter": "twitter.com/Dheerajmadhukar",
+    "github" : "github.com/Dheerajmadhukar"
+}
+
+console.log(object)
+
+/* OUTPUT [READ]
+> object
+{name: "Dheeraj Madhukar", twitter: "twitter.com/Dheerajmadhukar", github: "github.com/Dheerajmadhukar"}
+
+> object.github
+github: "github.com/Dheerajmadhukar"
+
+>object.name
+name: "Dheeraj Madhukar"
+twitter: "twitter.com/Dheerajmadhukar"
+
+*/
+
+/* OUTPUR [WRITE]
+
+> object.bio = "Hacker"
+Hacker
+
+> object
+{name: "Dheeraj Madhukar", twitter: "twitter.com/Dheerajmadhukar", github: "github.com/Dheerajmadhukar", bio: "Hacker"}
+
+*/
